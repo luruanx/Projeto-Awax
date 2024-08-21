d291efbab7cf61661d9c4265bc794b5dda772f4d
Ruan Richard <Richard_10luan@hotmail.com>
2024-08-21 17:29:29 -0300

    up nav ok

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,19 @@
 
 /* HEADER / NAV */
 
-var showNav = document.getElementById('menu-opener')
-var navshow = document.getElementById('display-nav')
-var closemenu = document.querySelectorAll('a')
-
-showNav.addEventListener("click", () => {
-    navshow.classList.toggle('navshow')
-})
+window.onload = function() {
+    document.querySelector(".menu-opener").addEventListener("click", function(){
+        if (document.querySelector(".menu nav").style.display == 'flex') {
+            document.querySelector(".menu nav").style.display = 'none';
+        } else {
+            document.querySelector(".menu nav").style.display = 'flex';
+        }
+    });
+    window.addEventListener('resize', function(){
+        if (window.innerWidth > 800){
+            document.querySelector(".menu nav").style.display = 'flex';
+        } else {
+            document.querySelector(".menu nav").style.display = 'none';
+        }
+    });
+};
